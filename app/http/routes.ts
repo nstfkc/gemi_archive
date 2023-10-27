dd1d320fdd8bba851500c26073197bad2b8c2729
Enes Tufekci <enesxtufekci@gmail.com>
2023-10-27 10:54:18 +0200

    cleanup

--- app/http/routes.ts
+++ app/http/routes.ts
@@ -3,10 +3,7 @@ import { Route } from "@/lib/server/Route";
 import { HomeController } from "./controllers/HomeController";
 import { AboutController } from "./controllers/AboutController";
 
-console.log("ROUTES");
-
 export const routes = {
   "/": Route.view("Home", [HomeController, "index"]),
   "/about": Route.view("About", [AboutController, "index"]),
-  "/x": Route.get(HomeController, "index"),
 };
