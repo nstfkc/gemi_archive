7bbc147e48cee53c6aecac8d457aacc987026d6c
Enes Tufekci <enesxtufekci@gmail.com>
2023-11-07 13:13:21 +0100

    clear old server files

--- server/dev.ts
+++ server/dev.ts
@@ -1,18 +1,16 @@
+import { Hono } from "hono";
+import { serve } from "@hono/node-server";
+
 import { readFileSync } from "node:fs";
 import path, { join } from "node:path";
-import express, { Router } from "express";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
 
 const rootDir = path.resolve(process.cwd());
 const libDir = path.join(rootDir, "lib");
 const appDir = path.join(rootDir, "app");
 const dbDir = path.join(rootDir, "db");
 
-export async function createServer(root = process.cwd()) {
-  const app = express();
-  app.use(cookieParser());
-  app.use(bodyParser.json());
+export async function main() {
+  const root = process.cwd();
 
   const vite = await (
     await import("vite")
@@ -20,11 +18,18 @@ export async function createServer(root = process.cwd()) {
     root,
     logLevel: "error",
     server: {
-      middlewareMode: true,
       watch: {
         usePolling: true,
-        interval: 1000,
+        interval: 100,
       },
+      hmr: {
+        clientPort: 5174,
+      },
+    },
+    build: {
+      ssrManifest: true,
+      manifest: true,
+      ssrEmitAssets: true,
     },
     appType: "custom",
     resolve: {
@@ -36,34 +41,44 @@ export async function createServer(root = process.cwd()) {
     },
   });
 
-  app.use(vite.middlewares);
+  vite.watcher.on("change", (path) => {
+    console.log(path);
+  });
 
-  app.use(async (req, res, next) => {
-    try {
+  const app = new Hono();
+
+  app.use(
+    "*",
+    async (ctx, next) => {
+      const res = await fetch(`http://localhost:5174/${ctx.req.path}`, {});
+      if (res.ok) {
+        return res;
+      } else {
+        await next();
+      }
+    },
+    async (ctx) => {
       const template = await vite.transformIndexHtml(
-        req.url,
+        ctx.req.url,
         readFileSync(path.resolve(join(rootDir, "index.html")), "utf-8"),
       );
-
       const { bootstrap } = await vite.ssrLoadModule(
         "/lib/server/bootstrap.tsx",
       );
+      const router = bootstrap(template);
 
-      const router = bootstrap(template) as Router;
-      return router(req, res, next);
-    } catch (error) {
-      const e = error as Error;
-      vite.ssrFixStacktrace(e);
-      console.log(e.stack);
-      return res.status(500).end(e.stack);
-    }
+      return await router.fetch(ctx.req.raw);
+    },
+  );
+
+  await vite.listen(5174);
+
+  serve({
+    fetch: app.fetch,
+    port: 5173,
+  }).on("listening", () => {
+    console.log("Server is running");
   });
-
-  return { app };
 }
 
-createServer().then(({ app }) => {
-  app.listen(5173, () => {
-    console.log("http://localhost:5173");
-  });
-});
+await main();
