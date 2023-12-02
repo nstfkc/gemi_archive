bfe76de9108493178feb799c4143ec18f0e15fd5
Enes Tufekci <enesxtufekci@gmail.com>
2023-12-02 19:26:12 +0100

    clean up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import typescript from "@rollup/plugin-typescript";
 
 const rootDir = path.resolve(process.cwd());
 const libDir = path.join(rootDir, "lib");
